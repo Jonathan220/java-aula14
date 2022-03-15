a5485230b4ad44612ce5fad4528492409548f949
Jonathan Abreu <abreu.jonathan220@gmail.com>
2022-03-15 20:37:54 -0300

    Good solution
    Solução que faz uso de tratamento de exceção e cria sua própria classe exception

---
+++ src/model/exceptions/DomainException.java
@@ -0,0 +1,12 @@
+package model.exceptions;
+
+public class DomainException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5947915182316068068L;
+
+	public DomainException(String msg) {
+		super(msg);
+	}
+}
